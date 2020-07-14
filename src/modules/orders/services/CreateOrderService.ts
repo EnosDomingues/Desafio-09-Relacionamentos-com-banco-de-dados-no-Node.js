efc840ecaa20fe7c373eb171a94316f5c3436ab0
Enos Domingues <enosdomingues@gmail.com>
2020-07-14 18:10:31 -0300

    Tests: 10 passed, 10 total

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,43 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('User does not exists');
+    }
+
+    const orderProducts = await this.productsRepository.findAllById(products);
+
+    const finalProducts = orderProducts.map(orderProduct => {
+      const finalProduct = products.find(
+        productFind => productFind.id === orderProduct.id,
+      );
+
+      return {
+        product_id: orderProduct.id,
+        price: orderProduct.price,
+        quantity: finalProduct?.quantity || 0,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: finalProducts,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
